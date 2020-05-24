adfc150bed7dad99660cd7a2253eaadb3b3094d5
leexiaoboo <leexiaoboo@outlook.com>
2020-05-24 12:46:32 +0800

    新的布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,6 @@
 const app = getApp()
 
 Page({
-
   data: {
     background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     indicatorDots: true,
@@ -11,5 +10,23 @@ Page({
     autoplay: true,
     interval: 2000,
     duration: 500
+  },
+  onLoad: function (options) {
+    wx.login({
+      success (res) {
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'http://49.232.106.88:8080/jeecg-boot/sys/getCheckCode', 
+            success (res1) {
+              console.log(res.code)
+              console.log(res1)
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
   }
 })
